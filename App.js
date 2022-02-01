9432b041311702169377ed9cf10e2f0d7669f076
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2022-02-01 11:42:10 +0200

    commit

--- App.js
+++ App.js
@@ -1,25 +1,65 @@
 import React from "react";
-import Posts from './components/Posts'
 import './App.css'
 
 export default class App extends React.Component {
   state = {
-    posts: [
-      {id: 1, name: 'JavaScript'},
-      {id: 2, name: 'ReactJS'},
-      {id: 3, name: 'VueJS'},
-    ]
+    count: 0,
+    isCounting: false,
   };
 
-  removePost = (id) => {
-    this.setState({posts: this.state.posts.filter(post => post.id !== id)})
+  componentDidMount() {
+    console.log('componentDidMount');
+    const userCount = localStorage.getItem("timer")
+    if(userCount) {
+      this.setState({count: +userCount})
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+    localStorage.setItem("timer", this.state.count)
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
+    clearInterval(this.counterId)
+  }
+
+  handleStart = () => {
+    this.setState({isCounting: true})
+
+    this.counterId = setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000) 
+  }
+
+  handleStop = () => {
+    this.setState({isCounting: false})
+    clearInterval(this.counterId)
+  }
+
+  handleReset = () => {
+    this.setState({isCounting: false, count: 0})
+    clearInterval(this.counterId)
   }
 
   render() {
-    const {posts} = this.state
     return (
       <div className="App">
-        <Posts posts={posts} removePost={this.removePost} />
+        <p>React Timer</p>
+        <p>{this.state.count}</p>
+        {!this.state.isCounting ? (
+          <button className="success" onClick={this.handleStart}>
+            Start
+          </button>
+        ) : (
+          <button className="danger" onClick={this.handleStop}>
+            Stop
+          </button>
+        )}
+        <button className="secondary" onClick={this.handleReset}>
+          Reset
+        </button>
       </div>
     );
   }
--- components/PostList.js
+++
@@ -1,9 +0,0 @@
-export default function PostList(props) {
-  const {name, id, removePost} = props
-  return (
-    <div className="todo">
-      <h4>{name}</h4>
-      <button className="danger" onClick={() => removePost(id)}>Delete</button>
-    </div>
-  )
-}
